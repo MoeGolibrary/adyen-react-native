b314edbcd53c9c37806ba42e878814862ee0af94
ivan <ivan@moego.pet>
2024-11-27 14:46:33 +0800

    feat: 添加adyen card和adyen terminal相关framework

---
+++ meogoPart/ios/AdyenPOS/Debug/ADYPOS.framework/Headers/ADYPOS.h
@@ -0,0 +1,18 @@
+//
+//  ADYPOS.h
+//  ADYPOS
+//
+//  Copyright (c) 2023 Adyen N.V.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ADYPOS.
+FOUNDATION_EXPORT double ADYPOSVersionNumber;
+
+//! Project version string for ADYPOS.
+FOUNDATION_EXPORT const unsigned char ADYPOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ADYPOS/PublicHeader.h>
+
+
